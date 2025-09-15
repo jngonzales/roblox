6acf5e5f755329978d2483ac34046f0202f73b60
Jobby Noel Cortez Gonzales <123081317+jngonzales@users.noreply.github.com>
2025-09-15 11:46:42 +0800

    Update comment for final combined version

--- script.lua
+++ script.lua
@@ -1,5 +1,5 @@
 -- LocalScript (put in StarterPlayer > StarterPlayerScripts)
--- FINAL COMBINED VERSION  ddaa
+-- FINAL COMBINED VERSION  ddaaa
 
 -- Services
 local Players = game:GetService("Players")
